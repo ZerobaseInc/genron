82d0c1243148b910a709b4ce21885a7068002b1d
Hideto Ishibashi <hideto@zerobase.jp>
2023-03-15 17:05:12 +0900

    Heroku設定修正

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: drush config:import --source=../config && drush cache:rebuild
+release: bin/heroku-postdeploy.sh && drush config:import --source=../config && drush cache:rebuild
 web: vendor/bin/heroku-php-apache2 web/
--- bin/heroku-postdeploy.sh
+++ bin/heroku-postdeploy.sh
@@ -1,8 +1,8 @@
 !/bin/sh
 echo "Running post-deploy script."
-echo "Importing database..."
 export DB_HOST=$STACKHERO_MARIADB_HOST
 export DB_PASS=$STACKHERO_MARIADB_ROOT_PASSWORD
+echo "Importing database..."
 drush sql:cli < data/dump.sql
 echo "Database imported."
 echo "Finishing post-deploy script."
